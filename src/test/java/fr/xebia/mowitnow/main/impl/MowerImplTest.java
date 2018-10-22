6d5e4d0a2d998f5c91d9eaf912698e937877a4bd
Mehdi <mehdi.mohand.cherif@cgi.com>
2018-10-22 15:00:21 +0200

    Adding UT

--- src/test/java/fr/xebia/mowitnow/main/impl/MowerImplTest.java
+++ src/test/java/fr/xebia/mowitnow/main/impl/MowerImplTest.java
@@ -1,30 +1,64 @@
-//package fr.xebia.mowitnow.main.impl;
-//
-//import org.junit.jupiter.api.Test;
-//import org.junit.platform.runner.JUnitPlatform;
-//import org.junit.runner.RunWith;
-//
-//@RunWith(JUnitPlatform.class)
-//public class MowerImplTest {
-//
-//	@Test
-//	public void testPivoterADroite() throws Exception {
-//		throw new RuntimeException("not yet implemented");
-//	}
-//
-//	@Test
-//	public void testPivoterAGauche() throws Exception {
-//		throw new RuntimeException("not yet implemented");
-//	}
-//
-//	@Test
-//	public void testAvancer() throws Exception {
-//		throw new RuntimeException("not yet implemented");
-//	}
-//
-//	@Test
-//	public void testExecution() throws Exception {
-//		throw new RuntimeException("not yet implemented");
-//	}
-//
-//}
+package fr.xebia.mowitnow.main.impl;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import fr.xebia.mowitnow.enums.InstructionEnum;
+import fr.xebia.mowitnow.enums.OrientationEnum;
+import fr.xebia.mowitnow.pojo.Position;
+
+@RunWith(JUnitPlatform.class)
+public class MowerImplTest {
+
+	@Test
+	public void testPivoterADroite() throws Exception {
+		MowerImpl mower = getMower();
+		mower.pivoterADroite();
+		
+		Assertions.assertEquals(new Position(0, 0, OrientationEnum.EAST), mower.getPosition());
+	}
+
+	@Test
+	public void testPivoterAGauche() throws Exception {
+		MowerImpl mower = getMower();
+		mower.pivoterAGauche();
+		
+		Assertions.assertEquals(new Position(0, 0, OrientationEnum.WEST), mower.getPosition());
+	}
+
+	@Test
+	public void testAvancer() throws Exception {
+		MowerImpl mower = getMower();
+		mower.avancer();
+		
+		Assertions.assertEquals(new Position(0, 1, OrientationEnum.NORTH), mower.getPosition());
+	}
+
+	@Test
+	public void testExecution() throws Exception {
+		MowerImpl mower = getMower();
+		mower.setInstructions(getInstructions());
+		mower.execution();
+		
+		Assertions.assertEquals(new Position(1, 0, OrientationEnum.NORTH), mower.getPosition());
+	}
+
+	private MowerImpl getMower() {
+		
+		return new MowerImpl(1, new Position(0, 0, OrientationEnum.NORTH), new Position(2, 2, OrientationEnum.NORTH), null);
+	}
+	
+	private Queue<InstructionEnum> getInstructions() {
+		Queue<InstructionEnum> instructions = new LinkedList<>();
+		instructions.add(InstructionEnum.A_DROITE);
+		instructions.add(InstructionEnum.AVANCER);
+		instructions.add(InstructionEnum.A_GAUCHE);
+		
+		return instructions;
+	}
+}
